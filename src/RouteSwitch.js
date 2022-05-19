4194937f50594456d7bf9f958a20affd22b726da
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-19 09:34:36 -0400

    added apple to RouteSwitch and added an apple route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
-
+import Apple from "./Apple";
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<App></App>}/>
             <Route path="/profile" element={<Profile></Profile>}/>
+            <Route path="/apple" element={<Apple></Apple>}
         </Routes>
         </BrowserRouter>
     )
