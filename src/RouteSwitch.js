83692db02c0536d8915ead3588666d9567e9fe70
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-19 09:27:19 -0400

    imported RouteSwitch component from the lesson

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,3 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
+
+const RouteSwitch = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App></App>}/>
+            <Route path="/profile" element={<Profile></Profile>}/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+export default RouteSwitch;
